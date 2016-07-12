1486dd0565a9710bafffb8c484abeeddfde82218
a-mellor <alexander.mellor@gmail.com>
2016-07-12 16:57:28 +0100

    add restaurant name validation and error message

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -32,6 +32,17 @@ feature 'restaurants' do
 			expect(page).to have_content 'KFC'
 			expect(current_path).to eq '/restaurants'
 	 	end
+
+      context 'an invalid restaurant' do
+        it 'does not let you submit a name that is too short' do
+          visit '/restaurants'
+          click_link 'Add a restaurant'
+          fill_in 'Name', with: 'kf'
+          click_button 'Create Restaurant'
+          expect(page).not_to have_css 'h2', text: 'kf'
+          expect(page).to have_content 'error'
+        end
+      end
 	 end
 
 	 context 'viewing restarants' do
@@ -65,18 +76,19 @@ feature 'restaurants' do
 
 	context 'deleting restaurants' do
 
-	before do
-		Restaurant.create(name: 'KFC', description: 'fried chicken')
-	end
+  	before do
+  		Restaurant.create(name: 'KFC', description: 'fried chicken')
+  	end
 
-  scenario 'removes a restaurant when a user clicks a delete link' do
-    visit '/restaurants'
-    click_link 'Delete KFC'
-    expect(page).not_to have_content 'KFC'
-    expect(page).to have_content 'Restaurant deleted successfully'
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
   end
 
-end
+
 
 
 end

b651feb57ad2a32840edd3796ba45b0d7b9eafb6
Carlos <chdezmar@gmail.com>
2016-07-12 12:08:43 +0100

    Added different route for each restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,24 @@ class RestaurantsController < ApplicationController
 		@resturants = Restaurant.all
 	end
 
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def create
+		@restaurant = Restaurant.create(restaurant_params)
+		redirect_to '/restaurants'
+	end
+
+	def show
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	private
+
+	def restaurant_params
+		params.require(:restaurant).permit(:name)
+	end
+
+
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -21,4 +21,27 @@ feature 'restaurants' do
        expect(page).not_to have_content('No resturants yet')
      end
    end
+
+	 context 'creating restaurants' do
+	 	scenario 'prompts user to fill out a form, then displays the new restaurant' do
+	 		visit '/restaurants'
+			click_link 'Add a restaurant'
+			fill_in 'Name', with: 'KFC'
+			click_button 'Create Restaurant'
+			expect(page).to have_content 'KFC'
+			expect(current_path).to eq '/restaurants'
+	 	end
+	 end
+
+	 context 'viewing restarants' do
+	 	let!(:kfc){ Restaurant.create(name:'KFC') }
+
+		scenario 'lets a user view restaurant' do
+			visit '/restaurants'
+			click_link 'KFC'
+			expect(page).to have_content('KFC')
+			expect(current_path).to eq "/restaurants/#{kfc.id}"
+		end
+	 end
+
 end
